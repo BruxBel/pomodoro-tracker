14c24369ca21c7ddd5d3dd05c653bd5e439275ae
BruxBel <aleksei.geek.rus@gmail.com>
2025-07-01 22:18:27 +0300

    add comment to gunicorn.conf.py

--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -3,3 +3,5 @@ from multiprocessing import cpu_count
 workers = (2 * cpu_count()) + 1
 worker_class = "uvicorn.workers.UvicornWorker"
 bind = "127.0.0.1:8000"
+
+# pip install uvloop httptools Это ускорит работу Uvicorn-воркеров.
